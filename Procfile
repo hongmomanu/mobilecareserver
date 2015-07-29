27cd48227ff90cc4541efb1e81b8a0cf91a5b9ea
jack <hongmomanu@163.com>
2015-07-29 13:39:16 +0800

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/mobilecareserver.jar clojure.main -m mobilecareserver.core
---
+++ test/mobilecareserver/test/db/core.clj
@@ -0,0 +1,33 @@
+(ns mobilecareserver.test.db.core
+  (:require [clojure.test :refer :all]
+            [mobilecareserver.db.core :as db]
+            [clojure.java.jdbc :as jdbc]
+            [mobilecareserver.db.migrations :as migrations]))
+
+(use-fixtures
+  :once
+  (fn [f]
+    (db/connect!)
+    (migrations/migrate ["migrate"])
+    (f)))
+
+(deftest test-users
+  (jdbc/with-db-transaction [trans-conn @db/conn]
+    (jdbc/db-set-rollback-only! trans-conn)
+    (is (= 1 (db/run
+               db/create-user!
+               {:id         "1"
+                :first_name "Sam"
+                :last_name  "Smith"
+                :email      "sam.smith@example.com"
+                :pass       "pass"}
+               trans-conn)))
+    (is (= [{:id         "1"
+             :first_name "Sam"
+             :last_name  "Smith"
+             :email      "sam.smith@example.com"
+             :pass       "pass"
+             :admin      nil
+             :last_login nil
+             :is_active  nil}]
+           (db/run db/get-user {:id "1"} trans-conn)))))
